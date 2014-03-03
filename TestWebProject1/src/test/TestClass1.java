97d421498b6e7672de14cfd112586bf2f92dedca
Cesar Ivan Orozco Cervantes <Cesar Ivan Orozco Cervantes>
2014-03-03 17:05:56 +0000

    Fixed bug 712

--- TestWebProject1/src/test/TestClass1.java
+++ TestWebProject1/src/test/TestClass1.java
@@ -3,6 +3,8 @@ package test;
 public class TestClass1 {
 
   public static void main(String[] args) {
+  	System.out.println("Starting...");
+  	
     for(String token : args) {
       System.out.println("--> " + token);
     }
