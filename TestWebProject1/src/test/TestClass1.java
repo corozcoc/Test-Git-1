963088cdb8986d7a4a3174a08d0b4a2b1cacb5e8
corozcoc <corozcoc@mx1.ibm.com>
2014-03-07 15:42:20 -0600

    Fixed defect 712

--- TestWebProject1/src/test/TestClass1.java
+++ TestWebProject1/src/test/TestClass1.java
@@ -5,6 +5,7 @@ public class TestClass1 {
   public static void main(String[] args) {
     for(String token : args) {
       System.out.println("--> " + token);
+      System.out.println("--> A");
     }
   }
 }
