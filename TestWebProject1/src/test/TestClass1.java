09be3a1292d16f0939170f510f615b49c1e5c057
corozcoc <corozcoc@mx1.ibm.com>
2014-03-07 14:23:43 -0600

    Fixed defect 712

--- TestWebProject1/src/test/TestClass1.java
+++ TestWebProject1/src/test/TestClass1.java
@@ -3,7 +3,7 @@ package test;
 public class TestClass1 {
 
   public static void main(String[] args) {
-  	System.out.println("Starting2...");
+  	System.out.println("Starting...");
   	
     for(String token : args) {
       System.out.println("--> " + token);
